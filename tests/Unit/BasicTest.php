956b5586ae241cc98d5fa6c4011ee3af2fdd89bd
PANDORA013 <thomas.aruan22@student.uisi.ac.id>
2025-06-27 20:07:45 +0700

    Perbaikan testing dan konfigurasi Pusher

---
+++ tests/Unit/BasicTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class BasicTest extends TestCase
+{
+    public function test_basic_example()
+    {
+        $this->assertTrue(true);
+    }
+}
